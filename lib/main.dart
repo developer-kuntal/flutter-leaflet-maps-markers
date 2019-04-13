db77249f35a9d483d454903ae311df358aca79e4
developer-kuntal <kuntal.iiest@gmail.com>
2019-04-13 21:01:17 +0530

    Flutter Leaflet Maps update at 9.01 PM 4/13/2019

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong/latlong.dart';
 
 void main() {
   runApp(new MaterialApp(
@@ -21,12 +22,51 @@ class _HomePageState extends State<HomePage> {
       ),
       body: new FlutterMap(
         options: new MapOptions(
+          center: new LatLng(40.71, -74.00),
           minZoom: 10.0
         ),
         layers: [
           new TileLayerOptions(
             urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
                 subdomains: ['a', 'b', 'c']
+          ),
+          new MarkerLayerOptions(
+            markers: [
+              new Marker(
+                width: 45.0,
+                height: 45.0,
+                point: new LatLng(40.71, -74.00),
+                builder: (context) => new Container(
+                  child: IconButton(
+                    icon: Icon(Icons.location_on),
+                    color: Colors.red,
+                    iconSize: 45.0,
+                    onPressed: () {
+                      print("Marker Tapped");
+                    },
+                  ),
+                )
+              )
+            ]
+          ),
+           new MarkerLayerOptions(
+            markers: [
+              new Marker(
+                width: 45.0,
+                height: 45.0,
+                point: new LatLng(40.71, -74.02),
+                builder: (context) => new Container(
+                  child: IconButton(
+                    icon: Icon(Icons.location_on),
+                    color: Colors.green,
+                    iconSize: 45.0,
+                    onPressed: () {
+                      print("Marker2 Tapped");
+                    },
+                  ),
+                )
+              )
+            ]
           )
         ],
       ),
