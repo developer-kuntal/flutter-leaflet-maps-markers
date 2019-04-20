d094f90d956dd8bf8274ab1d904e54c777b27f12
developer-kuntal <kuntal.iiest@gmail.com>
2019-04-20 14:34:33 +0530

    Flutter Leaflet Map updated at 2.34 PM 4/20/2019

--- android/app/src/main/java/com/example/flutter_leaflet_maps/MainActivity.java
+++ android/app/src/main/java/com/example/flutter_leaflet_maps/MainActivity.java
@@ -11,3 +11,78 @@ public class MainActivity extends FlutterActivity {
     GeneratedPluginRegistrant.registerWith(this);
   }
 }
+
+// import android.support.annotation.NonNull;
+// import android.support.v7.app.AppCompatActivity;
+// import android.os.Bundle;
+// import com.mapbox.mapboxsdk.Mapbox;
+// import com.mapbox.mapboxsdk.maps.MapView;
+// import com.mapbox.mapboxsdk.maps.MapboxMap;
+// import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+// import com.mapbox.mapboxsdk.maps.Style;
+ 
+// public class MainActivity extends AppCompatActivity {
+//   private MapView mapView;
+//   @Override
+//   protected void onCreate(Bundle savedInstanceState) {
+//     super.onCreate(savedInstanceState);
+//     Mapbox.getInstance(this, "pk.eyJ1Ijoia3VudGFsLTEyMyIsImEiOiJjanVvamp5NGIwcWxzNDRwcDRjajlzaHo1In0.UunnVckVv5bHh0xtHl0QoA");
+//     setContentView(R.layout.activity_main);
+//     mapView = findViewById(R.id.mapView);
+//     mapView.onCreate(savedInstanceState);
+
+//     mapView.getMapAsync(new OnMapReadyCallback() {
+//     @Override
+//     public void onMapReady(@NonNull MapboxMap mapboxMap) {
+//       mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
+//         @Override
+//         public void onStyleLoaded(@NonNull Style style) {
+//     // Map is set up and the style has loaded. Now you can add data or make other map adjustments
+//         }
+//       });
+//     }
+//   });
+// }
+ 
+// @Override
+// public void onStart() {
+//   super.onStart();
+//   mapView.onStart();
+// }
+ 
+// @Override
+// public void onResume() {
+//   super.onResume();
+//   mapView.onResume();
+// }
+ 
+// @Override
+// public void onPause() {
+//   super.onPause();
+//   mapView.onPause();
+// }
+ 
+// @Override
+// public void onStop() {
+//   super.onStop();
+//   mapView.onStop();
+// }
+ 
+// @Override
+// public void onLowMemory() {
+//   super.onLowMemory();
+//   mapView.onLowMemory();
+// }
+ 
+// @Override
+// protected void onDestroy() {
+//   super.onDestroy();
+//   mapView.onDestroy();
+// }
+ 
+// @Override
+// protected void onSaveInstanceState(Bundle outState) {
+//   super.onSaveInstanceState(outState);
+//   mapView.onSaveInstanceState(outState);
+// }
+// }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong/latlong.dart';
+import 'package:geolocator/geolocator.dart';
 
 void main() {
   runApp(new MaterialApp(
+    debugShowCheckedModeBanner: false,
     home: new HomePage(),
   ));
 }
@@ -14,62 +16,110 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  bool mapToggle = false;
+  Geolocator _geolocator = new Geolocator();
+
+  Future<GeolocationStatus>_getPermission() async {
+    final GeolocationStatus result = await _geolocator.checkGeolocationPermissionStatus();
+    return result;
+  }
+
+  Future<Position>_getLocation() {
+    return _getPermission().then((result) async{
+      Position coords = await _geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.best
+      );
+      return coords;
+    });
+  }
+
+  Future<Position>_buildMap() async{
+    return _getLocation().then((response) {
+      return response;
+    });
+  }
+
+  // @override
+  void initState() {
+    super.initState();
+    setState(() {
+      mapToggle = true; 
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+
+    var futureBuilder = new FutureBuilder(
+      future: _buildMap(), //_myStream(),
+      builder: (BuildContext context, AsyncSnapshot<Position> snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.none:
+            return Text('Press button to start.');
+          case ConnectionState.active:
+          case ConnectionState.waiting:
+            return Text('Awaiting result...');
+          case ConnectionState.done:
+            if (snapshot.hasError)
+              return Text('Error: ${snapshot.error}');
+            if (snapshot.hasData) {
+              return new FlutterMap(
+                  options: new MapOptions(
+                    center: new LatLng(snapshot.data.latitude, snapshot.data.longitude),
+                    minZoom: 10.0
+                  ),
+                  layers: [
+                    new TileLayerOptions(
+                      urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                          subdomains: ['a', 'b', 'c']
+                    ),
+                    new MarkerLayerOptions(
+                      markers: [
+                        new Marker(
+                          width: 45.0,
+                          height: 45.0,
+                          point: new LatLng(snapshot.data.latitude, snapshot.data.longitude),
+                          builder: (context) => new Container(
+                            child: IconButton(
+                              icon: Icon(Icons.location_on),
+                              color: Colors.red,
+                              iconSize: 45.0,
+                              onPressed: () {
+                                print("Marker Tapped");
+                              },
+                            ),
+                          )
+                        )
+                      ]
+                    ),
+                  ]
+              );
+            }
+        }
+    },);
+
     return Scaffold(
       appBar: new AppBar(
-        title: new Text('Leaflet Page'),
+        title: new Text('Leaflet Map'),
       ),
-      body: new FlutterMap(
-        options: new MapOptions(
-          center: new LatLng(40.71, -74.00),
-          minZoom: 10.0
-        ),
-        layers: [
-          new TileLayerOptions(
-            urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-                subdomains: ['a', 'b', 'c']
-          ),
-          new MarkerLayerOptions(
-            markers: [
-              new Marker(
-                width: 45.0,
-                height: 45.0,
-                point: new LatLng(40.71, -74.00),
-                builder: (context) => new Container(
-                  child: IconButton(
-                    icon: Icon(Icons.location_on),
-                    color: Colors.red,
-                    iconSize: 45.0,
-                    onPressed: () {
-                      print("Marker Tapped");
-                    },
+      body: new Column(
+        children: <Widget>[
+          Stack( children: <Widget>[
+            Container(
+                    height: MediaQuery.of(context).size.height - 100.0,
+                    width: double.infinity,
+                    // Wrap with stream builder could be sloved your problem...
+                    child: mapToggle ? futureBuilder : Center(child: 
+                        Text('Loading...Please wait', style: TextStyle(
+                          fontSize: 20.0
+                        ),
+                      ),
+                    )
                   ),
-                )
-              )
-            ]
-          ),
-           new MarkerLayerOptions(
-            markers: [
-              new Marker(
-                width: 45.0,
-                height: 45.0,
-                point: new LatLng(40.71, -74.02),
-                builder: (context) => new Container(
-                  child: IconButton(
-                    icon: Icon(Icons.location_on),
-                    color: Colors.green,
-                    iconSize: 45.0,
-                    onPressed: () {
-                      print("Marker2 Tapped");
-                    },
-                  ),
-                )
-              )
-            ]
-          )
+          ], )
         ],
-      ),
+      )
+        
     );
   }
 }
